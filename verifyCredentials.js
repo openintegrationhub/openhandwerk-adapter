4f06f5a96e8cf58e76191297c1d374b1f13e4ed7
Crowd <incepted@outlook.de>
2019-10-02 19:53:35 +0200

    final test

--- verifyCredentials.js
+++ verifyCredentials.js
@@ -14,10 +14,7 @@ async function getToken(config) {
 
   try {
     const tokenRequest = await request.post(options);
-    console.log("[REQ]");
-    console.log(tokenRequest);
     const { success } = tokenRequest;
-    console.log(success);
     return success;
   } catch (err) {
     // console.log(`ERROR: ${err}`);
@@ -37,7 +34,6 @@ async function verifyCredentials(credentials, cb) {
     };
 
     const token = await getToken(cfg);
-    console.log("TOKEN: -> ", token);
 
     if (token) {
       cb(null, { verified: true });
