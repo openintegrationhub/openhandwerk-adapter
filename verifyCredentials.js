f6c09bb34eb7c3481befe56fdc28e5263717f9ed
Crowd <incepted@outlook.de>
2019-10-02 19:25:29 +0200

    verifyCredentials final update

--- verifyCredentials.js
+++ verifyCredentials.js
@@ -3,8 +3,6 @@
 const request = require('request-promise');
 
 async function getToken(config) {
-  console.log("getting token..");
-
   const getTokenOptions = {
     uri: config.apiBaseUrl + `/login`,
     body: {
@@ -13,13 +11,7 @@ async function getToken(config) {
     }
   };
 
-  //const tokenRequest = await request.post(getTokenOptions);
-  console.log("request token..");
   return request.post(getTokenOptions);
-  //const tokenObject = JSON.parse(tokenRequest);
-  //const token = tokenObject.access_token;
-  //console.log("token retrieved: ", token);
-  //return token;
 }
 
 async function verifyCredentials(credentials, cb) {
@@ -32,28 +24,7 @@ async function verifyCredentials(credentials, cb) {
       apiBaseUrl: credentials.apiBaseUrl
     };
 
-    const token = getToken(cfg);
-
-    console.log("token got: - ", token);
-
-    return token
-      .then(function(token) {
-        console.log("[PROMISE ANSWER - TOKEN]", token);
-        if (token !== false) {
-          cb(null, { verified: true });
-          console.log('Credentials verified successfully');
-          credentials.apiKey = token;
-          return true;
-        } else {
-          throw new Error('Error in validating credentials!');
-        }
-      })
-      .catch(function() {
-        console.log("credentials invalid")
-        cb(null, { verified: false });
-      });
-
-
+    return getToken(cfg);
   } catch (e) {
     console.log("Error again");
     console.log(`${e}`);
